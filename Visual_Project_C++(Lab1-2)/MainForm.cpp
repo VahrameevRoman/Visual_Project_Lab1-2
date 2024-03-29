d82291ae5f8b94642adb6d99368d8d410910d9db
student-422 <student-422@CT422-ARM-8>
2024-03-29 13:20:50 +0300

    Добавьте файлы проекта.

---
+++ Visual_Project_C++(Lab1-2)/MainForm.cpp
@@ -0,0 +1,15 @@
+#include "MainForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+using namespace std;
+
+[STAThreadAttribute]
+int main(array<String^>^)
+{
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::EnableVisualStyles();
+	VisualProjectCLab12::MainForm form;
+	Application::Run(% form);
+
+	return 0;
+}
---
+++ Visual_Project_C++(Lab1-2)/MainForm.h
@@ -0,0 +1,59 @@
+#pragma once
+
+namespace VisualProjectCLab12 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MainForm
+	/// </summary>
+	public ref class MainForm : public System::Windows::Forms::Form
+	{
+	public:
+		MainForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MainForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(300,300);
+			this->Text = L"MainForm";
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
+#pragma endregion
+	};
+}
